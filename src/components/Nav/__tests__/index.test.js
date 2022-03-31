7377d939b7911e27851546282d994a0b8ada2a7f
KJ Jackson <kjjackson619@yahoo.com>
2022-03-30 23:31:11 -0400

    gallery tests

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,22 +3,40 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
+const categories = [
+    { name: 'portraits', description: 'Portraits of people in my life' }
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+
 afterEach(cleanup);
 
 describe('Nav component', () => {
     it('renders', () => {
-        render(<Nav />);
+        render(<Nav
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+        />);
     });
 
     it('matches snapshot', () => {
-        const { asFragment } = render(<Nav />);
+        const { asFragment } = render(<Nav
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+        />);
         expect(asFragment()).toMatchSnapshot();
     });
 })
 
 describe('emoji is visible', () => {
     it('inserts emoji into the h2', () => {
-        const { getByLabelText } = render(<Nav />);
+        const { getByLabelText } = render(<Nav
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+        />);
         expect(getByLabelText('camera')).toHaveTextContent('📸');
     });
 })
